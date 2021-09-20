7d881acbaec2c0ad2a95d62419b441028344690a
abidmujkic <abidmujkic1@gmail.com>
2021-09-20 15:06:28 +0200

    ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,13 @@
+function reusableFunction() {
+    console.log("Hi Padawan!");
+
+    //Only change code below this line
+
+    
+
+    //Only change code above this line
+
+    
+}
+reusableFunction();
+module.exports = reusableFunction;
