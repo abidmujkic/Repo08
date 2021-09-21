79ef3591f79e3da39bdc515839d2b6123c0c33c0
abidmujkic <abidmujkic1@gmail.com>
2021-09-21 05:57:24 +0200

    ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+
+function reusablePrintInfo(parameter1, parameter2) {
+    console.log(parameter1, parameter2);
+
+}
+
+reusablePrintInfo("John", "23");
+reusablePrintInfo("Michael", "43");
+
+module.exports = reusablePrintInfo;
